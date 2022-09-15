b3452264fa68d898d25a1eaae8a4b6669747bb4f
egon-isnoise <egonisnoise@proton.me>
2022-09-15 16:19:26 +0200

    done, could fix some css and maybe add a picture?

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import './App.css';
+import { useState } from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {addUser, deleteUser, updateUsername} from "./features/Users";
+
+function App() {
+
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  
+  const [name, setName] =useState("");
+  const [username, setUsername] =useState("");
+  const [newUsername, setNewUsername] =useState("");
+
+  return (
+    <div className="App">
+      <div className="addUser">
+        <input 
+          type="text" 
+          placeholder='Name...' 
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input 
+          type="text" 
+          placeholder='Username...' 
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+
+        <button 
+          onClick={() => {
+            dispatch(addUser(
+              {id: userList.length +1, name: name, username: username}
+            ))
+          }}
+        >
+          Add User
+        </button>
+        
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h3> {user.name} </h3>
+              <h1> {user.username} </h1>
+              {/* <h3> {user.id} </h3> */}
+
+              <input 
+                type="text" 
+                placeholder='New Username...' 
+                onChange={(event) => {
+                  setNewUsername(event.target.value);
+                }}
+              />
+              
+              <button 
+                onClick={() => {
+                  dispatch(updateUsername(
+                    {id:user.id, username: newUsername}
+                  ))
+                }}
+              >
+                Update Username
+              </button>
+
+              <button 
+                onClick={() => {
+                  dispatch(deleteUser({id:user.id}))
+                }}
+              >
+                Delete User
+              </button>
+              
+            </div>
+          ) 
+        })}
+      </div> 
+    </div>
+  );
+}
+
+export default App;
